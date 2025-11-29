2d638b78e737041b5c82a9e979734362abef7716
abjii <156369372+abjii@users.noreply.github.com>
2025-11-29 13:49:56 +0100

    bugfix. second number on multi digits

--- calculator.js
+++ calculator.js
@@ -23,29 +23,34 @@ function operate(operator, a, b) {
     case "/":
       return divide(a, b);
     default:
-      throw new Error("Invalid operator");
+      return "Invalid operator";
   }
 }
 
 const buttons = document.querySelectorAll("button");
 const input = document.querySelector("input");
 
-function reset() {
-  let x = (y = operator = input.value = "");
-}
+let x = (y = operator = input.value = "");
+let readyForSecondNumber = false;
 
-reset();
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
     if (["+", "-", "*", "/"].includes(button.value)) {
       operator = button.value;
       x = Number(input.value);
+      readyForSecondNumber = true;
     } else if (button.value === "=") {
       y = Number(input.value);
       input.value = operate(operator, x, y);
-    } else if (button.value === "clear") reset();
-    else {
-      if (operator != "" && x != "") input.value = "";
+      readyForSecondNumber = false;
+    } else if (button.value === "clear") {
+      x = y = operator = input.value = "";
+      readyForSecondNumber = false;
+    } else {
+      if (readyForSecondNumber) {
+        input.value = "";
+        readyForSecondNumber = false;
+      }
       input.value += button.value;
     }
   });
